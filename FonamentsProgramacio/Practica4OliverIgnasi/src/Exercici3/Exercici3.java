81786f697341962276824688a99735e0edbd2fe7
ignasioliver <ignasioliverf@gmail.com>
2016-11-24 18:04:26 +0100

    Exercise 3 & 4

--- FonamentsProgramacio/Practica4OliverIgnasi/src/Exercici3/Exercici3.java
+++ FonamentsProgramacio/Practica4OliverIgnasi/src/Exercici3/Exercici3.java
@@ -4,6 +4,152 @@ import Keyboard.*;
 
 public class Exercici3 {
 	public static void main (String args[]){
+		int  n, numero, quinDigit = 0, suma = 0, resultatnumDigits;
+		System.out.println("Indica el numero del que vols saber la suma dels seus digits: ");
+		numero = Keyboard.readInt();
+		//amb resultatnumDigits evitem que per cada iteracio es cridi la funcio numDigits
+		resultatnumDigits = numDigits(numero);
+		for (n = 0; n < resultatnumDigits; n++){
+			suma = suma + (digit(numero, quinDigit));
+			quinDigit++;
+		}System.out.println("La suma dels digits es: " + suma);
+	}
+	
+	public static int numDigits (int numero){
+		int digits;
+		for (digits = 0; numero > 0; digits++) numero = numero / 10;
+		return digits;	
+	}
+	
+	public static int digit (int numero, int quinDigit){
+		int comptador;
+		for (comptador = 1; comptador <= quinDigit; comptador++){
+			numero = numero / 10;
+		}numero = numero % 10;
+		return numero;		
+	}	
+}
+
+
+
+
+
+
+
+
+/*
+
+package Exercici3;
+
+import Keyboard.*;
+
+public class Exercici3 {
+	public static void main (String args[]){
+		System.out.println("Per la funcio 1 (numDigits)");
+		int parametre;
+		parametre = Keyboard.readInt();
+		System.out.println(numDigits(parametre));
+		
+		
+		System.out.println("Per la funcio 2 (digit)");
+		int numero, quinDigit;
+		numero = Keyboard.readInt();
+		quinDigit  = Keyboard.readInt();
+		System.out.println(digit(numero, quinDigit));
 		
 	}
+	
+	private static int numDigits (int numero){
+		int digits;
+		for (digits = 0; numero > 0; digits++) numero = numero / 10;
+		return digits;
+		
+	}
+	
+	private static int digit (int numero, int quinDigit){
+		int comptador;
+		for (comptador = 1; comptador <= quinDigit; comptador++){
+			numero = numero / 10;
+		}numero = numero % 10;
+		return numero;
+		
+	}
+	
 }
+
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+FUNCIONA PERFECTE:
+package Exercici3;
+
+import Keyboard.*;
+
+public class Exercici3 {
+	public static void main (String args[]){
+		System.out.println("Per la funcio 1 (numDigits)");
+		int parametre;
+		parametre = Keyboard.readInt();
+		System.out.println(numDigits(parametre));
+		
+		int valorimportatdelafuncio1 = numDigits(parametre);
+		
+		
+		System.out.println("Per la funcio 2 (digit)");
+		int  n, numero, quinDigit;
+		n = 0;
+		numero = Keyboard.readInt();
+		quinDigit  = 0;
+		int suma = 0;
+		while (n < valorimportatdelafuncio1){
+			suma = suma + (digit(numero, quinDigit));
+			quinDigit++;
+			n++;
+		}System.out.println(suma);
+		
+	}
+	
+	private static int numDigits (int numero){
+		int digits;
+		for (digits = 0; numero > 0; digits++) numero = numero / 10;
+		return digits;
+		
+	}
+	
+	private static int digit (int numero, int quinDigit){
+		int comptador;
+		for (comptador = 1; comptador <= quinDigit; comptador++){
+			numero = numero / 10;
+		}numero = numero % 10;
+		return numero;
+		
+	}
+	
+}
+
+
+*/
+
+
+
+
--- FonamentsProgramacio/Practica4OliverIgnasi/src/Exercici4/Exercici4.java
+++ FonamentsProgramacio/Practica4OliverIgnasi/src/Exercici4/Exercici4.java
@@ -1,9 +1,97 @@
 package Exercici4;
 
 import Keyboard.*;
+import Exercici3.*;
 
 public class Exercici4 {
 	public static void main (String args[]){
+		int any, mes, dia;
+		long data;
+		System.out.println("Numerologia");
+		// Per tal de tenir el programa ben organitzat, cridem les funcions per parts
+		any = lecturaany(1910, 2015);
+		mes = lecturames(1, 12);
+		dia = lecturadia(mes, any);
+		data = any * 10000 + (mes * 100) + dia;
+		numeroSort(data);
+	}
+	
+	public static int lecturaany (int min, int max){
+		int any;
+		System.out.println("Indica l'any de naixement [" + min + ", " + max + "]");
+		any = Keyboard.readInt();
+		while (any < min || any > max){
+			System.out.println("Any incorrecte. Indica novament l'any de naixament [" + min + ", " + max + "]");
+			any = Keyboard.readInt();
+		}
+		return any;
+	}
+	
+	public static int lecturames (int min, int max){
+		int mes;
+		System.out.println("Indica el mes de naixement [" + min + ", " + max + "]");
+		mes = Keyboard.readInt();
+		while (mes < min || mes > max){
+			System.out.println("Mes incorrecte. Indica novament el mes de naixament [" + min + ", " + max + "]");
+			mes = Keyboard.readInt();
+		}
+		return mes;
+	}
+	
+	public static boolean bisiesto (int any){
+		boolean bolea = false;
+		if (any % 4 == 0){
+			if (any % 100 != 0) bolea = true; //es de traspas
+			else if (any % 100 == 0){
+				if (any % 400 == 0) bolea = true;
+				else bolea = false;
+			}
+		}else bolea = false; //false = not traspas	
+		return bolea;
+	}
+	/*
+	Podriem tenir un sol parametre a "lecturadia", nomes el mes, i que sigui aquesta
+	mateixa funcio la que cridi l'any i el demani, pero seria un embolic ja que si
+	nomes es es volgues llegir el dia, tambe llegiria l'any per defecte. 
+	D'aquesta manera no nomes es mante mes organitzat, sino que evitem que la funcio
+	faci una altra crida
+	*/
+	public static int lecturadia (int mes, int any){
+		int dia, max;
+		boolean comprovacio = bisiesto(any);
+		System.out.println("Indica el dia de naixement [1, 28] o [1, 29] o [1, 30] o [1, 31] en funcio del mes");
+		dia = Keyboard.readInt();
+		if (mes == 1 || mes == 3 || mes == 5 || mes == 7 || mes == 8 || mes == 10 || mes == 12){//dia maxim;
+			max = 31;
+		}else if (mes == 2){
+			if (comprovacio ==  true) max = 29;
+			else max = 28;
+		}else max = 30;
+		if (comprovacio == true) System.out.println("Es any de traspas");
+		while (dia > max){
+			System.out.println("Dia incorrecte. Indica novament dia de naixement [1, " + max + "]");
+			dia = Keyboard.readInt();
+		}
+		return dia;
+	}
+	
+	public static void numeroSort (long data){
+		int nombretotaldigits, nombresort = 0, dataint, posiciodigit; //canviar nombretotaldigits per posiciodigit
+		dataint = (int) data;
+		nombretotaldigits = Exercici3.numDigits(dataint);
+		for (posiciodigit = nombretotaldigits; posiciodigit >= 0; posiciodigit--){ //igualem posicio per ferho mes clar i per l'if que vindra
+			nombresort = nombresort + Exercici3.digit(dataint, posiciodigit);
+			/*
+			Es possible que aqui "nombresort" tingui dos digits, per tant els separem.
+			Sabem tambe que no en pot tenir mes de 2, de manera que no cal que cridem
+			cap funcio, fem l'operacio directament per estalviar-nos temps d'execucio
+			*/
+			if (nombresort > 9){
+			nombresort = nombresort/10 + nombresort%10;
+			}
+		}
+		System.out.println("El teu numero es " + nombresort);
 		
 	}
+	
 }
